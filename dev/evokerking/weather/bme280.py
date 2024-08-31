6624a53831cd61ab192a5008a7db3ed7813ac2ea
evokerking1 <118303818+evokerking1@users.noreply.github.com>
2024-08-31 17:15:53 -0400

    Try to fix things

--- dev/evokerking/weather/bme280.py
+++ dev/evokerking/weather/bme280.py
@@ -9,7 +9,7 @@ address = 0x77 # Adafruit BME280 address. Other BME280s may be different
 bus = smbus2.SMBus(port)
 
 while True:
-    bme280_data = bme280.sample(bus,address)
+
     humidity  = bme280_data.humidity
     pressure  = bme280_data.pressure
     ambient_temperature = bme280_data.temperature

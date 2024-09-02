0f22611c9c1c9344ee3e1880bb00e929e5906023
evokerking1 <118303818+evokerking1@users.noreply.github.com>
2024-09-02 18:24:26 -0400

    Readied integration for stuff

--- dev/evokerking/weather/bme280_sensor.py
+++ dev/evokerking/weather/bme280_sensor.py
@@ -1,6 +1,5 @@
 import bme280 # Import the BME280 module
 import smbus2 # Import the SMBus module of the I2C library
-from time import sleep # Import the sleep function from the time module
 
 
 # Create the BME280 sensor object
@@ -9,11 +8,12 @@ from time import sleep # Import the sleep function from the time module
 port = 1
 address = 0x77 # Adafruit BME280 address. Other BME280s may be different
 bus = smbus2.SMBus(port)
+bme280.load_calibration_params(bus, address)
 
-while True:
+def read_bme280():
     bme280_data = bme280.sample(bus, address)
     humidity  = bme280_data.humidity
     pressure  = bme280_data.pressure
     ambient_temperature = bme280_data.temperature
     print(humidity, pressure, ambient_temperature)
-    sleep(1)
+    return humidity, pressure, ambient_temperature
